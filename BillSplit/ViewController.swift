58a0bf704feed4e2d4924b0696a8a62a7dd33f92
Ashutosh Mishra <amdevprojects@gmail.com>
2021-10-04 12:26:51 +0530

    project created

---
+++ BillSplit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BillSplit
+//
+//  Created by Ashutosh on 04/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
